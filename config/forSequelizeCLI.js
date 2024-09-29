a65d88bc37fbbf5ec73d0f59d651efcd53e32726
hashirsyed <itshashirsyed@gmail.com>
2024-09-29 07:51:05 +0500

    Corrected some code

--- config/forSequelizeCLI.js
+++ config/forSequelizeCLI.js
@@ -6,4 +6,11 @@ module.exports = {
   username: config.get("db.username"),
   password: config.get("db.password"),
   host: config.get("db.host"),
+  dialectOptions: {
+    charset: 'utf8mb4',  // Set the charset to utf8mb4 for full Unicode support
+  },
+  define: {
+    charset: 'utf8mb4',   // Ensure tables and columns use utf8mb4
+    collate: 'utf8mb4_unicode_ci',  // Set the collation to utf8mb4_unicode_ci for proper Unicode sorting
+  },
 };
