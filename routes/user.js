b38e9cef965ab6b0d70d27df7180d1177194bab7
hashirsyed <itshashirsyed@gmail.com>
2024-11-02 10:01:29 +0500

    Message added

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,9 @@ const controller = require("../controllers/user");
 
 // Routes
 router.post("/", controller.signUp);
+router.get("/", (req,res)=>{
+    res.status(200).send("Message from the backend")
+});
 router.post("/login", controller.login);
 
 module.exports = router;
