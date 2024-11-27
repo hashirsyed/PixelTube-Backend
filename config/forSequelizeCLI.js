e7167853a367777b5dd4115ed9f4395bb0dc5363
hashirsyed <itshashirsyed@gmail.com>
2024-11-27 16:51:53 +0500

    SOmething changed

--- config/forSequelizeCLI.js
+++ config/forSequelizeCLI.js
@@ -6,11 +6,13 @@ module.exports = {
   username: config.get("db.username"),
   password: config.get("db.password"),
   host: config.get("db.host"),
+  port: config.get("db.custom_port"),
   dialectOptions: {
-    charset: 'utf8mb4',  // Set the charset to utf8mb4 for full Unicode support
+    charset: "utf8mb4", // Set the charset to utf8mb4 for full Unicode suppor
+    connectTimeout: 60000,
   },
   define: {
-    charset: 'utf8mb4',   // Ensure tables and columns use utf8mb4
-    collate: 'utf8mb4_unicode_ci',  // Set the collation to utf8mb4_unicode_ci for proper Unicode sorting
+    charset: "utf8mb4", // Ensure tables and columns use utf8mb4
+    collate: "utf8mb4_unicode_ci", // Set the collation to utf8mb4_unicode_ci for proper Unicode sorting
   },
 };
